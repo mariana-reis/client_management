eb0c032dc324a0adf0eb00ff5a59c2277d3d752b
mariana-reis <mariana.reis@al.resilia.com.br>
2023-10-29 20:16:04 -0300

    :sparkles: feat: navBar component

---
+++ resources/views/components/nav.blade.php
@@ -0,0 +1,7 @@
+<nav class="bg-white fixed w-full z-20 top-0 left-0 border-b border-gray-200">
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    <a class="flex items-center">
+        <img src="{!! asset('img/img-cnpj.svg') !!}"  class="h-8 mr-3" alt="CNPJ Logo">
+        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-black">Gerenciamento de Clientes</span>
+    </a>
+</nav>
